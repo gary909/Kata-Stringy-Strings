792e40cc3c869aa441fa95a26da69a9c50bd800e
gary <gary.white@lodgit.com>
2022-01-17 13:31:46 +0100

    code working

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-function stringy(size) {
-    // your code here   
+function stringy(size) { 
+    let myStr = ""; // emptry string
+    for (var i = 0; i < size; i++){
+        myStr = myStr + '10'; // push '10' for every amount 
+    }
+    var halfStr = myStr.substring(0, size) // then half it
+    return halfStr;
 }
 
-console.log(string(3)); // 101
-console.log(string(6)); // 101010
-console.log(string(4)); // 1010
-console.log(string(12)); // 101010101010
+console.log(stringy(3)); // 101
+console.log(stringy(6)); // 101010
+console.log(stringy(4)); // 1010
+console.log(stringy(12)); // 101010101010

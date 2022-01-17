bda2d7be8dc47d263f4c380daa3a1adcacc76f1d
gary <gary.white@lodgit.com>
2022-01-17 13:09:43 +0100

    template

---
+++ index.js
@@ -0,0 +1,8 @@
+function stringy(size) {
+    // your code here   
+}
+
+console.log(string(3)); // 101
+console.log(string(6)); // 101010
+console.log(string(4)); // 1010
+console.log(string(12)); // 101010101010
